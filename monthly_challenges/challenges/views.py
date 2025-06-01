bd20d6f3622bd3052ed3f76b3d8f4cb9fd437613
unknown <polluxrey@gmail.com>
2025-06-01 22:40:56 +0800

    Dynamic Path Segments & Captured Values

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
+month_list = ["january", "february", "march",
+              "april", "may", "june", "july", "august", "september", "october", "november", "december"]
 
-def january(request):
-    return HttpResponse("This is January.")
 
+def monthly_challenge(request, month):
+    challenge_text = None
 
-def february(request):
-    return HttpResponse("This is February.")
+    if month in month_list:
+        challenge_text = f"This is for the {month.capitalize()} Challenge page!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+
+    return HttpResponse(challenge_text)
