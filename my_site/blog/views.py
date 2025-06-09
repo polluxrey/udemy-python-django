31511ac935b2b5205f5339fc91424bcdde174903
unknown <polluxrey@gmail.com>
2025-06-10 02:02:53 +0800

    Updating project progress

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -3,6 +3,7 @@ import os
 from django.conf import settings
 from django.http import HttpResponseNotFound
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 INDEX_POSTS_LIMIT = 3
 
@@ -13,12 +14,14 @@ with open(file_path, "r", encoding="utf-8") as f:
     posts = json.load(f)
 
 # Load user profile
-file_path = os.path.join(settings.BASE_DIR, "static", "data", "user_profile.json")
+file_path = os.path.join(settings.BASE_DIR, "static",
+                         "data", "user_profile.json")
 
 with open(file_path, "r", encoding="utf-8") as f:
     user = json.load(f)
 
 # Create your views here.
+
 
 def index(request):
     context = {
@@ -27,6 +30,7 @@ def index(request):
     }
 
     return render(request, "index.html", context=context)
+
 
 def view_post(request, slug):
     post = None
@@ -40,7 +44,7 @@ def view_post(request, slug):
 
     if post is None:
         return HttpResponseNotFound("Article not found!")
-    
+
     if (slug != normalized_slug) and post:
         return redirect(request.path.lower(), permanent=True)
 
@@ -50,3 +54,26 @@ def view_post(request, slug):
     }
 
     return render(request, "post.html", context=context)
+
+
+def view_all_posts(request):
+    if len(posts) == 0:
+        return HttpResponseNotFound("No articles found!")
+
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(posts[::-1], 3)
+
+    try:
+        posts_by_page = paginator.page(page)
+    except PageNotAnInteger:
+        posts_by_page = paginator.page(1)
+    except EmptyPage:
+        posts_by_page = paginator.page(paginator.num_pages)
+
+    context = {
+        "user": user,
+        "posts": posts_by_page,
+    }
+
+    return render(request, "posts.html", context=context)
